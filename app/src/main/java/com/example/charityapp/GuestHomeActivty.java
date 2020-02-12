e7c7dd9bcc3e0d5f068664df47b97c00e979fcb4
jbates888 <jackbates555@gmail.com>
2020-02-12 17:07:58 -0600

    made guest activity

--- app/src/main/java/com/example/charityapp/GuestHomeActivty.java
+++ app/src/main/java/com/example/charityapp/GuestHomeActivty.java
@@ -1,33 +1,69 @@
 package com.example.charityapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.List;
+
 
 public class GuestHomeActivty extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    DatabaseReference ref;
+    FirebaseDatabase database;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_guest_home_activty);
+        setContentView(R.layout.activity_home);
 
         Toolbar toolbar = findViewById(R.id.appbar);
         setSupportActionBar(toolbar);
 
+
+        recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        database = FirebaseDatabase.getInstance();
+        ref = database.getReference("Events");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseRecyclerAdapter<Event, recycleAdapter> firebaseRecyclerAdapter =
+                new FirebaseRecyclerAdapter<Event, recycleAdapter>(Event.class, R.layout.row, recycleAdapter.class, ref){
+                    protected void populateViewHolder(recycleAdapter holder, Event event, int i){
+                        holder.setView(getApplicationContext(), event.getName(), event.getProgram(), event.getDate(), event.getTime());
+                    }
+                };
+        recyclerView.setAdapter(firebaseRecyclerAdapter);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.menu, menu);
+        menuInflater.inflate(R.menu.guestmenu, menu);
         return super.onCreateOptionsMenu(menu);
     }
 
@@ -35,10 +71,11 @@ public class GuestHomeActivty extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch (item.getItemId()){
             case R.id.ActionLogout:
+                Toast.makeText(getApplicationContext(), "User Logged out", Toast.LENGTH_LONG).show();
                 FirebaseAuth.getInstance().signOut();
                 finish();
                 startActivity(new Intent(this, MainActivity.class));
-
+                return true;
         }
         return true;
     }
