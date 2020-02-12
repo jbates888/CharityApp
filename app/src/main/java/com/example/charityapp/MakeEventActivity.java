050b491e7b4c3d5b52e3a4ed8e4e079af8e69b7a
jbates888 <jackbates555@gmail.com>
2020-02-12 00:52:15 -0600

    added ability activity to create a new event

---
+++ app/src/main/java/com/example/charityapp/MakeEventActivity.java
@@ -0,0 +1,76 @@
+package com.example.charityapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MakeEventActivity extends AppCompatActivity {
+
+    FirebaseDatabase mFirebasedatabase;
+    DatabaseReference mRefrence;
+    EditText Name, Program, Description, Date, Time;
+    int maxId = 0;
+    Button btn;
+
+    Event event;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_event);
+
+        Name = findViewById(R.id.name_edit);
+        Program = findViewById(R.id.program_edit);
+        Description = findViewById(R.id.description_edit);
+        Date = findViewById(R.id.date_edit);
+        Time = findViewById(R.id.time_edit);
+        btn = findViewById(R.id.create_btn);
+
+        event = new Event();
+
+        mRefrence = mFirebasedatabase.getInstance().getReference().child("Events");
+
+        mRefrence.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if(dataSnapshot.exists()){
+                    maxId = (int) dataSnapshot.getChildrenCount();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                event.setName(Name.getText().toString());
+                event.setProgram(Program.getText().toString());
+                event.setDescription(Description.getText().toString());
+                event.setDate(Date.getText().toString());
+                event.setTime(Time.getText().toString());
+                event.setFunding("$0");
+                event.setVolunteers("");
+
+                mRefrence.child(String.valueOf(maxId+1)).setValue(event);
+
+                Toast.makeText(getApplicationContext(), "Event Created", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+    }
+}
