e9db6258383175849c38df036c1b78ed2a04f143
jbates888 <jackbates555@gmail.com>
2020-02-25 14:10:32 -0600

    Added details screens so all users can click on and view the event details, buttons for the specific user arnt working yet.

---
+++ app/src/main/java/com/example/charityapp/GuestEventDetails.java
@@ -0,0 +1,41 @@
+package com.example.charityapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class GuestEventDetails extends AppCompatActivity {
+
+    TextView nameTxt;
+    TextView progTxt;
+    TextView descTxt;
+    TextView dateTxt;
+    TextView timeTxt;
+    TextView fundsTxt;
+    TextView volsTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest_event_details);
+
+        nameTxt = findViewById(R.id.event_details_title);
+        progTxt = findViewById(R.id.event_details_prog);
+        descTxt = findViewById(R.id.event_details_desc);
+        dateTxt = findViewById(R.id.event_details_date);
+        timeTxt = findViewById(R.id.event_details_time);
+        fundsTxt = findViewById(R.id.event_details_funds);
+        volsTxt = findViewById(R.id.event_details_vols);
+
+
+        Bundle extras = getIntent().getExtras();
+        nameTxt.setText( extras.getString("Name"));
+        progTxt.setText("Program: "  + extras.getString("Program"));
+        descTxt.setText("Description: "  +  extras.getString("Description"));
+        dateTxt.setText("Date: "  +  extras.getString("Date"));
+        timeTxt.setText("Time: "  +  extras.getString("Time"));
+        fundsTxt.setText("Funding: "  +  extras.getString("Funds"));
+        volsTxt.setText("Volunteers: "  +  extras.getString("Volunteers"));
+    }
+}
